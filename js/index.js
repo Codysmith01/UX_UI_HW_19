063b7591417c72d1674e10c304da1083e3b812f8
Codysmith01 <1980z28cs@gmail.com>
2022-10-15 19:38:06 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+    $("li").hover(function(){
+      $(this).css("color", "#5374B8");
+    }, function() {
+      $(this).css("color", "black");
+    }, function() {     
+    });
+
+    $("button").hover(function(){
+      $(this).css("color", "#5374B8");
+    }, function() {
+      $(this).css("color", "black");
+    }, function() {     
+    });
+
+   
